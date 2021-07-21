54b3ad0edb1d61ae30ced920699daf5662c88ede
Mélina <purple.melinaa@gmail.com>
2021-07-21 16:40:05 +0200

    V2 soucis de centrage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import "./App.css";
+import { useState } from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const addCount = () => {
+    const newCounter = counter +1;
+    setCounter(newCounter);
+  }
+
+  const lessCount = () => {
+    const newCounter = counter -1;
+    setCounter(newCounter);
+  }
+
+  const resetCount = () => {
+    const newCounter = 0;
+    setCounter(newCounter);
+  }
+
   return (
     <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
+    <div className="App">
+      <Header />
+      <div className="counter">
+  <div className="line">
+      <div className="btn-plus-minus" onClick={lessCount} style={{ visibility: counter >= 1 ? "visible" : "hidden"}}>-</div>
+   
+     <div className="counter-count">{counter}</div> 
+     <div className="btn-plus-minus" onClick={addCount} style={{ visibility: counter < 10 ? "visible" : "hidden"}} >+</div>
+      </div>
+      </div>
+      <div className="btn-reset" onClick={resetCount}>Reset</div>
+
+    <Footer />
+    </div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+  return <div className="header">
+      <div className="logo">
+          <i class="fa fa-calculator fa-3x"></i>
+      </div>
+      <div className="title">React Counter</div>
+  </div>;
+};
+
+export default Header;
